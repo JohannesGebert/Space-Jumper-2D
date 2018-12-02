e5966f52df0beddc3828628bce9e6f9acc4ad312
Gerrit Letz <gerrit.letz@googlemail.com>
2018-12-02 13:51:47 +0100

    wand und gegner

---
+++ Assets/Scripts/EnemyControllerScript.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControllerScript : MonoBehaviour {
+
+  enum EnemyStates { moveLeft = 0, moveRight = 1, moveStop = 2, jumpAir = 3, enemyDie = 4, goHome = 5 }
+  public float MoveSpeed = 20.0f;
+
+  EnemyStates enemyState = EnemyStates.moveLeft;
+
+  float returnHomeRange = 4.0f;
+  Transform homePosition;
+  float deathForce = 3.0f;
+
+  EnemyStates currentState;
+  Vector3 velocity = Vector3.zero;
+
+  float gravity = 20;
+  
+  bool isRight = false;
+
+  Vector3 myTransform;
+  float resetMoveSpeed = 0.0f;
+
+  SpriteRenderer spriteRenderer;
+  Animator animator;
+
+
+  public bool CanJump = false;
+	// Use this for initialization
+	void Start () {
+    Vector3 myTransform = transform.position;
+    
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    animator = GetComponent<Animator>();
+  }
+
+
+  // Update is called once per frame
+  void Update () {
+    //if(controller.isGrounded)
+    {
+      switch(enemyState)
+      {
+        case EnemyStates.moveLeft:
+          PatrolLeft();
+          break;
+        case EnemyStates.moveRight:
+          PatrolRight();
+          break;
+        case EnemyStates.moveStop:
+          if(isRight)
+          {
+            IdleRight();
+          }
+          else
+          {
+            IdleLeft();
+          }
+          
+          break;
+      }
+    }
+
+    
+    //aniPlay.SetFloat("Speed", Mathf.Abs(Move));
+    //velocity.y -= gravity * Time.deltaTime;
+  
+
+    GetComponent<Rigidbody2D>().velocity = velocity;
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+
+  }
+
+  void PatrolRight()
+  {
+    velocity.x = MoveSpeed * Time.deltaTime;
+    animator.SetFloat("Walk", Mathf.Abs(MoveSpeed));
+    spriteRenderer.flipX = !isRight;
+    isRight = true;
+  }
+  void PatrolLeft()
+  {
+    velocity.x = -MoveSpeed * Time.deltaTime;
+    animator.SetFloat("Walk", Mathf.Abs(MoveSpeed));
+    spriteRenderer.flipX = !isRight;
+    isRight = false;
+  }
+
+  void IdleRight()
+  {
+    velocity.x = 0;
+    animator.SetFloat("Walk", Mathf.Abs(0));
+    isRight = true;
+  }
+  void IdleLeft()
+  {
+    velocity.x = 0;
+    animator.SetFloat("Walk", Mathf.Abs(0));
+    isRight = false;
+  }
+
+  void DieRight()
+  {
+    velocity.x = 0;
+    new WaitForSeconds(0.1f);
+    animator.SetBool("Dead", true);
+    new WaitForSeconds(0.4f);
+    //destroy gameobject
+  }
+}
---
+++ Assets/Scripts/MoveWall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour {
+
+  public Transform Target;
+  Vector3 velocity = Vector3.zero;
+  public float SmoothTime = 50f;
+
+  public float StepWidth = 10f;
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector3 TargetPos = Target.position;
+
+    TargetPos.x = TargetPos.x + StepWidth;
+    
+    TargetPos.z = transform.position.z;
+    TargetPos.y = transform.position.y;
+
+    transform.position = Vector3.SmoothDamp(transform.position, TargetPos, ref velocity, SmoothTime);
+  }
+}
