69d3614cbca8816a410908a4bce0deee5aca40af
Gerrit Letz <gerrit.letz@googlemail.com>
2018-12-06 17:22:42 +0100

    prefabs

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    /*if (other.tag == "Obstacle")
+    {
+      enemyState = EnemyStates.moveStop;
+      new WaitForSeconds(0.4f);
+      if (isRight)
+      {
+        enemyState = EnemyStates.moveLeft;
+      }
+      else
+      {
+        enemyState = EnemyStates.moveRight;
+      }
+    }*/
+  }
+}
