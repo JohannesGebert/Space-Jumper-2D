c318ff85a711bc7ade60d943895a3cc26600c450
JohannesGebert <JohannesGebert@users.noreply.github.com>
2018-12-30 19:53:01 +0100

    Nahkampf

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,17 +6,42 @@ public class Weapon : MonoBehaviour {
 
     public Transform firePoint;
     public GameObject bulletPrefab;
+
+  private float timeBtwAttack;
+  public float startTimeBtwAttack;
+
+  public Transform attackPosition;
+  public LayerMask whatIsEnemy;
+  public float attackRange;
+  public int Damage;
+
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire1") && PauseMenu.GameIsPaused == false)
-        {
-            Shoot();
-        }
+    if (Input.GetButtonDown("Fire1") && PauseMenu.GameIsPaused == false)
+    {
+      Shoot();
+    }
+
+    if (Input.GetKey(KeyCode.LeftShift))
+    {
+      timeBtwAttack = startTimeBtwAttack;
+      Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemy);
+      for (int i = 0; i < enemiesToDamage.Length; i++)
+      {
+        enemiesToDamage[i].GetComponent<EnemyControllerScript>().TakeDamage(Damage);
+      }
+    }
 	}
 
-    void Shoot()
-    {
-        //shooting Logic
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-    }
+  void OnDrawGizmosSelected()
+  {
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+  }
+
+  void Shoot()
+  {
+     //shooting Logic
+    Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+  }
 }
