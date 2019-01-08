fa920c0e40c3a045cb790ca749e97b5dc8841f44
ChristopherLeicht <44893603+ChristopherLeicht@users.noreply.github.com>
2019-01-08 23:34:51 +0100

    Revert "Schussmechanik, Asteroiden, Spielrand, etc."
    This reverts commit 2593f20141d12ae2337f332ea125f75f354bec01.

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+    public float scrollspeed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x += Time.deltaTime / scrollspeed;
+
+        mat.mainTextureOffset = offset;
+	}
+}
