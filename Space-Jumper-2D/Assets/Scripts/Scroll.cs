2593f20141d12ae2337f332ea125f75f354bec01
ChristopherLeicht <44893603+ChristopherLeicht@users.noreply.github.com>
2019-01-08 23:16:25 +0100

    Schussmechanik, Asteroiden, Spielrand, etc.

---
+++ Space-Jumper-2D/Assets/Scripts/Scroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+    public float scrollspeed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.y += Time.deltaTime / scrollspeed;
+
+        mat.mainTextureOffset = offset;
+	}
+}
---
+++ Space-Jumper-2D/Assets/SpaceBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceBullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public int damage = 120;
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        EnemyControllerScript enemy = hitInfo.GetComponent<EnemyControllerScript>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+
+}
